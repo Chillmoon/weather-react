f78f76772bfa4cba1bd4b33700f3b45fd6a99bc1
Chillmoon <108537775+Chillmoon@users.noreply.github.com>
2022-08-06 17:43:56 +0300

    worked on api calls

--- src/Weather.js
+++ src/Weather.js
@@ -3,66 +3,68 @@ import "./Weather.css";
 import axios from "axios";
 
 export default function Weather() {
-  let [city, setCity] = useState(null);
-  let [temperature, setTemperature] = useState(null);
-  let [humidity, setHumidity] = useState(null);
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3a94f3778290bfeee61278505dbbe51d&units=metric`;
+  const [weatherData, setWeatherData] = useState({ ready: false });
 
   function showWeather(response) {
-    setTemperature(Math.round(response.data.main.temp));
-    setHumidity(response.data.main.humidity);
+    setWeatherData({
+      ready: true,
+      temperature: Math.round(response.data.main.temp),
+      city: "Smila",
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+      wind: response.data.wind.speed,
+    });
   }
 
-  function showTemperature(event) {
-    event.preventDefault();
+  function search() {
+    const apiKey = "3a94f3778290bfeee61278505dbbe51d";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=Smila&appid=${apiKey}&units=metric`;
     axios.get(url).then(showWeather);
   }
-  function changeCity(event) {
-    setCity(event.target.value);
-  }
 
-  return (
-    <div className="Weather">
-      <div className="SearchForm">
-        <form className="Form row" onSubmit={showTemperature}>
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter the city"
-              className="textInput form-control"
-              onChange={changeCity}
-              autoFocus="on"
-            />
-          </div>
-          <div className="col-3">
-            <input type="submit" value="Search" className="searchInput" />
-          </div>
-        </form>
-      </div>
-
-      <div className="cityInfo">
-        <h1>{city}</h1>
-        <ul>
-          <li>Tuesday 18:15</li>
-          <li>Cloudy</li>
-        </ul>
-        <div className="row weatherInfo">
-          <div className="col-6">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-              alt="cloudy"
-            />
-            <span className="temperature">{temperature}</span>
-            <span className="unit">°C</span>
-          </div>
-          <div className="col-6">
-            <ul>
-              <li>Humidity: {humidity}%</li>
-              <li>Wind: 10 km/h</li>
-            </ul>
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <div className="SearchForm">
+          <form className="Form row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter the city"
+                className="textInput form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input type="submit" value="Search" className="searchInput" />
+            </div>
+          </form>
+        </div>
+        <div className="cityInfo">
+          <h1>{weatherData.city}</h1>
+          <ul>
+            <li>Tuesday 18:15</li>
+            <li>{weatherData.description}</li>
+          </ul>
+          <div className="row weatherInfo">
+            <div className="col-6">
+              <img src={weatherData.icon} alt="cloudy" />
+              <span className="temperature">{weatherData.temperature}</span>
+              <span className="unit">°C</span>
+            </div>
+            <div className="col-6">
+              <ul>
+                <li>Humidity: {weatherData.humidity}%</li>
+                <li>Wind: {Math.round(weatherData.wind)} km/h</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    search();
+    return <h1>Loading...</h1>;
+  }
 }
