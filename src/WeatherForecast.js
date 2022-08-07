c438ff1a5d4408a060347ab322db99c1ce4d57b0
Chillmoon <108537775+Chillmoon@users.noreply.github.com>
2022-08-07 22:17:56 +0300

    little changes

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -8,6 +8,7 @@ export default function WeatherForecast(props) {
   let [forecast, setForecast] = useState(null);
 
   function handleResponse(response) {
+    console.log(response.data.daily);
     setForecast(response.data.daily);
     setLoaded(true);
   }
@@ -17,7 +18,19 @@ export default function WeatherForecast(props) {
       <div className="WeatherForecast">
         <div className="row">
           <div className="col">
-            <WeatherForecastDay data={forecast[0]} />
+            <WeatherForecastDay data={forecast[1]} />
+          </div>
+          <div className="col">
+            <WeatherForecastDay data={forecast[2]} />
+          </div>
+          <div className="col">
+            <WeatherForecastDay data={forecast[3]} />
+          </div>
+          <div className="col">
+            <WeatherForecastDay data={forecast[4]} />
+          </div>
+          <div className="col">
+            <WeatherForecastDay data={forecast[5]} />
           </div>
         </div>
       </div>
